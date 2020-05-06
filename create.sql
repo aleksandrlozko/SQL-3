70693e52a80ea07316362af395f40ffa085886e2
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-05-06 22:29:55 +0300

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,51 @@
+CREATE TABLE country (
+    country_name VARCHAR2(50) NOT NULL
+);
+
+ALTER TABLE country ADD CONSTRAINT country_pk PRIMARY KEY ( country_name );
+
+CREATE TABLE genres (
+    genres_name VARCHAR2(50) NOT NULL
+);
+
+ALTER TABLE genres ADD CONSTRAINT genres_pk PRIMARY KEY ( genres_name );
+
+CREATE TABLE movie (
+    name          VARCHAR2(50) NOT NULL,
+    release_year  NUMBER,
+    duration      NUMBER
+);
+
+ALTER TABLE movie ADD CONSTRAINT movie_pk PRIMARY KEY ( name );
+
+CREATE TABLE movie_country (
+    movie_name    VARCHAR2(50) NOT NULL,
+    country_name  VARCHAR2(50) NOT NULL
+);
+
+ALTER TABLE movie_country ADD CONSTRAINT movie_country_pk PRIMARY KEY ( country_name,
+                                                                        movie_name );
+
+CREATE TABLE movie_genres (
+    movie_name   VARCHAR2(50) NOT NULL,
+    genres_name  VARCHAR2(50) NOT NULL
+);
+
+ALTER TABLE movie_genres ADD CONSTRAINT movie_genres_pk PRIMARY KEY ( movie_name,
+                                                                      genres_name );
+
+ALTER TABLE movie_country
+    ADD CONSTRAINT movie_country_country_fk FOREIGN KEY ( country_name )
+        REFERENCES country ( country_name );
+
+ALTER TABLE movie_country
+    ADD CONSTRAINT movie_country_movie_fk FOREIGN KEY ( movie_name )
+        REFERENCES movie ( name );
+
+ALTER TABLE movie_genres
+    ADD CONSTRAINT movie_genres_genres_fk FOREIGN KEY ( genres_name )
+        REFERENCES genres ( genres_name );
+
+ALTER TABLE movie_genres
+    ADD CONSTRAINT movie_genres_movie_fk FOREIGN KEY ( movie_name )
+        REFERENCES movie ( name );
