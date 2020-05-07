e7fb2132b56ecfef590d3dfd96dd257a4d99cce8
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-05-07 16:05:26 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,14 +1,14 @@
 DECLARE
     TYPE tab_movie IS VARRAY(5) OF movie.name%TYPE;
-    TYPE tab_country IS VARRAY(5) OF country.country%TYPE;
+    TYPE tab_duration IS VARRAY(5) OF country.country%TYPE;
     val_movie tab_movie;
-    val_country tab_country;
+    val_duration tab_duration;
 BEGIN
     val_movie := tab_movie('American Horror Story', 'Elite', 'Money Heist');
-    val_country := tab_country('USA', 'Spain', 'Spain');
+    val_duration := tab_duration('9', '4', '4');
     FOR i IN 1 .. val_movie.count
     LOOP
-        INSERT INTO movie_country (movie_name, country_name) VALUES (val_movie(i), val_country(i));
+        INSERT INTO movie (name, duration) VALUES (val_movie(i), val_duration(i));
         COMMIT;
     END LOOP;
 END;
